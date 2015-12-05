236ea18ca0f8547b8ecb8aaa81070c735be6be48
Martin Cross <mncross@gmail.com>
2015-12-05 10:15:48 -0600

    multiple date/time pickers working

--- js/app.js
+++ js/app.js
@@ -1,1 +1,51 @@
-var app = angular.module('onyx', ['ui.bootstrap', 'ui.router']);
+var app = angular.module('onyx', ['ui.router', 'ui.bootstrap']);
+
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/');
+
+	$stateProvider
+		.state('case', {
+			url:'/',
+			templateUrl: 'partial/case.html',
+			controller: 'caseController'
+		});
+}]);
+
+app.controller('caseController', ['$scope', '$log', function ($scope, $log) {
+
+  console.log('Onyx is live!');
+
+
+/**
+ * ui.bootstrap datepicker
+ */
+  $scope.dateStatus = {
+    receivedDate: false,
+    decisionDate: false
+  };
+
+  $scope.today = function() {
+    $scope.receivedDate = new Date();
+    $scope.decisionDate = new Date();
+  };
+  $scope.today();
+
+  $scope.toggleOpen = function($event, which) {
+    $event.preventDefault();
+    $event.stopPropagation();
+
+    $scope.dateStatus[which] = ! $scope.dateStatus[which];
+  };
+
+  $scope.dateOptions = {
+    'year-format': "'yy'",
+    'starting-day': 1
+  };
+
+  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'shortDate'];
+  $scope.format = $scope.formats[0];
+  /**
+   * END datepicker
+   */
+
+}]);
