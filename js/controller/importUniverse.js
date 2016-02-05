9fc7b1f88a42717722ca568761c01ffcf74ba7ba
Martin Cross <mncross@gmail.com>
2016-02-04 21:02:55 -0600

    comment and manual hoist importUniverse.js

--- js/controller/importUniverse.js
+++ js/controller/importUniverse.js
@@ -3,7 +3,9 @@ app.controller('importUniverse', ['caseVault', function(caseVault) {
 
   self.universeType = null;
 
-
+  /**
+   * Field names for each universe type per CMS spec. This code will eventually be refactored to implement tab-delimited to JSON conversion. Would like to have a view that displays field headers and allows user to confirm/match to CMS standard header.
+   */
   self.universeTypes = {
     SCDER: {
       receivedDate: "Date the request was received",
@@ -119,6 +121,9 @@ app.controller('importUniverse', ['caseVault', function(caseVault) {
     }
   };
 
+  /**
+   * Labels for options in universe upload form
+   */
   self.labels = {
     caseType: null,
     priority: 'Standard or Expedited?',
@@ -127,6 +132,9 @@ app.controller('importUniverse', ['caseVault', function(caseVault) {
     extendApproval: 'Late Approval?'
   };
 
+  /**
+   * Method to change labels based on selections in universe upload form
+   */
   self.changeLabels = function(field) {
     switch (field) {
       case 'caseType': if(self.caseType === 'CD') {
@@ -157,54 +165,24 @@ app.controller('importUniverse', ['caseVault', function(caseVault) {
     }
   };
 
-
-/**
- * parseInputFile is a copy pasta function and uses the directive 'onReadFile'.
- */
-  self.parseInputFile = function(fileText, caseType, priority, exception, extendApproval, reimbursement){
-    // fileText = fileText.replace(/( )/g, '_');
-    var universeType = self.universeTypes[self.makeUniverseType(caseType, priority, exception, reimbursement)];
-
-    if (caseType === 'RD') {
-      exception = false;
-    }
-    if (reimbursement) {
-      exception = false;
-      priority = false;
-    }
-
-    // Need functionality to convert tab delimited to JSON. fileText is loading JSON file in meantime.
-    /**
-     * BUG: JSON.parse does not recognize CMS formatted times prior to 10:00:00 because the number begins with 0. Must be string.
-     */
-    var universe = JSON.parse(fileText);
-    // console.log("JSON.parse", universe);
-    universe.map(function(request, index, array) {
-      request.caseNum = index;
-      request.reimbursement = reimbursement;
-      request.exception = exception;
-      request.extendApproval = extendApproval;
-      request.caseType = caseType;
-      request.priority = priority;
-      request.drugName = request["Drug Name, Strength & Dosage Form"];
-      request.drugNDC = request.NDC_11;
-      request.beneficiaryHICN = request["Beneficiary HICN"];
-      request.decision = request["Was the case approved or denied?"];
-      request.receivedDate = self.CMSToDate(request[universeType.receivedDate], request[universeType.receivedTime]);
-      if (request[universeType.ssDate] !== null) {
-        request.ssDate = self.CMSToDate(request[universeType.ssDate], request[universeType.ssTime]);
-      }
-      request.decisionDate = self.CMSToDate(request[universeType.decisionDate], request[universeType.decisionTime]);
-      request.effectuationDate = self.CMSToDate(request[universeType.effectuationDate], request[universeType.effectuationTime]);
-      request.oralNotificationDate = self.CMSToDate(request[universeType.oralNotificationDate], request[universeType.oralNotificationTime]);
-      request.writtenNotificationDate = self.CMSToDate(request[universeType.writtenNotificationDate], request[universeType.writtenNotificationTime]);
-      // "Disposition of the request" is also a field that may need to be evaluated
-      request = caseVault.setDueDate(request);
-      return request;
-    });
-    caseVault.setUniverse(universe);
+  /**
+   * Method to convert CMS formatted date/time to Date object
+   */
+  self.CMSToDate = function(dateInput, timeInput) {
+    dateInput = dateInput.toString().split("");
+    timeInput = timeInput.toString().split("");
+    var year = dateInput.splice(0,4).join('');
+    var month = dateInput.splice(0,2).join('');
+    var day = dateInput.splice(0,2).join('');
+    var hour = timeInput.splice(0,2).join('');
+    var minute = timeInput.splice(0,2).join('');
+    var second = timeInput.splice(0,2).join('');
+    return new Date(year, month - 1, day, hour, minute, second);
   };
 
+  /**
+   * Method to decide universe type based on selections in universe upload form
+   */
   self.makeUniverseType = function(caseType, priority, exception, reimbursement) {
     console.log('makeUniverseType run');
     if (caseType === 'CD') {
@@ -245,16 +223,50 @@ app.controller('importUniverse', ['caseVault', function(caseVault) {
     return self.universeType;
   };
 
-  self.CMSToDate = function(dateInput, timeInput) {
-    dateInput = dateInput.toString().split("");
-    timeInput = timeInput.toString().split("");
-    var year = dateInput.splice(0,4).join('');
-    var month = dateInput.splice(0,2).join('');
-    var day = dateInput.splice(0,2).join('');
-    var hour = timeInput.splice(0,2).join('');
-    var minute = timeInput.splice(0,2).join('');
-    var second = timeInput.splice(0,2).join('');
-    return new Date(year, month - 1, day, hour, minute, second);
+  /**
+   * parseInputFile is a copy pasta function and uses the directive 'onReadFile'.
+   */
+  self.parseInputFile = function(fileText, caseType, priority, exception, extendApproval, reimbursement){
+    // fileText = fileText.replace(/( )/g, '_');
+    var universeType = self.universeTypes[self.makeUniverseType(caseType, priority, exception, reimbursement)];
+
+    if (caseType === 'RD') {
+      exception = false;
+    }
+    if (reimbursement) {
+      exception = false;
+      priority = false;
+    }
+
+    // Need functionality to convert tab delimited to JSON. fileText is loading JSON file in meantime.
+    /**
+     * BUG: JSON.parse does not recognize CMS formatted times prior to 10:00:00 because the number begins with 0. Must be string.
+     */
+    var universe = JSON.parse(fileText);
+    universe.map(function(request, index, array) {
+      request.caseNum = index;
+      request.reimbursement = reimbursement;
+      request.exception = exception;
+      request.extendApproval = extendApproval;
+      request.caseType = caseType;
+      request.priority = priority;
+      request.drugName = request["Drug Name, Strength & Dosage Form"];
+      request.drugNDC = request.NDC_11;
+      request.beneficiaryHICN = request["Beneficiary HICN"];
+      request.decision = request["Was the case approved or denied?"];
+      request.receivedDate = self.CMSToDate(request[universeType.receivedDate], request[universeType.receivedTime]);
+      if (request[universeType.ssDate] !== null) {
+        request.ssDate = self.CMSToDate(request[universeType.ssDate], request[universeType.ssTime]);
+      }
+      request.decisionDate = self.CMSToDate(request[universeType.decisionDate], request[universeType.decisionTime]);
+      request.effectuationDate = self.CMSToDate(request[universeType.effectuationDate], request[universeType.effectuationTime]);
+      request.oralNotificationDate = self.CMSToDate(request[universeType.oralNotificationDate], request[universeType.oralNotificationTime]);
+      request.writtenNotificationDate = self.CMSToDate(request[universeType.writtenNotificationDate], request[universeType.writtenNotificationTime]);
+      // "Disposition of the request" is also a field that may need to be evaluated
+      request = caseVault.setDueDate(request);
+      return request;
+    });
+    caseVault.setUniverse(universe);
   };
 
 }]);
