70e76dd91059ceacb8f5bb542b2029421d7cb1d7
Martin Cross <mncross@gmail.com>
2016-02-05 13:23:08 -0600

    comment universeDetailController.js

--- js/controller/universeDetailController.js
+++ js/controller/universeDetailController.js
@@ -1,5 +1,9 @@
 app.controller('universeDetailController', ['universeCaseNum', 'universeCase', function(universeCaseNum, universeCase) {
   var self = this;
+
+  /**
+   * This controller is the basis for the scope created by universeController.js, but currently serves little other purpose.
+   */
   self.universeCaseNum = universeCaseNum;
   self.universeCase = universeCase;
   console.log("universeCaseNum", universeCaseNum);
